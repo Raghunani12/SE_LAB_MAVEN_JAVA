afa8d28b10b38a4e12e5461325f1b2a850c287e3
RAGHU <thisisraghunani@gmail.com>
2024-11-25 13:25:55 +0530

    MAVEN_JAVA

---
+++ src/main/java/com/example/my_maven_project33/App.java
@@ -0,0 +1,10 @@
+package com.example.my_maven_project33;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("MAVEN JAVA PROJECT ");
+    }
+}
